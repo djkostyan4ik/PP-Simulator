e91f0828e81be77ffbdc0cc7dfe2357c7971e0a1
djkostyan4ik <kostyamalinovskiy16@gmail.com>
2024-10-29 16:37:52 +0100

    Stwory

---
+++ Simulator/Animals.cs
@@ -0,0 +1,13 @@
+﻿namespace Simulator;
+
+public class Animals
+{
+    public required string Description { get; init; }
+    public uint Size { get; set; } = 3;
+
+
+    // Info property readonly
+    public string Info => $"{Description} <{Size}>";
+
+}
+
---
+++ Simulator/Creature.cs
@@ -0,0 +1,32 @@
+﻿namespace Simulator;
+ public class Creature
+ {
+
+    // Automatic properties
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+
+    // Constructor that accepts initial values ​​for Name and Level
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+
+    // Constructor parameterless
+    public Creature()
+    { }
+
+
+    // SayHi method
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi, I'm {Name}, my level is {Level}!");
+    }
+
+
+    // Readonly property Info
+    public string Info => $"{Name} [{Level}]";
+}
