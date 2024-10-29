083b6b7be975d7d7acdd05dee534136f5ce7c58b
djkostyan4ik <kostyamalinovskiy16@gmail.com>
2024-10-29 17:43:15 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -1,8 +1,41 @@
-﻿namespace Simulator;
+﻿using System.Xml.Linq;
+
+namespace Simulator;
 
 public class Animals
 {
-    public required string Description { get; init; }
+    // Default value "Unknown"
+    private string _description = "Unknown";
+    public string Description 
+    {
+        get => _description;
+        set
+        {
+            // Cannot have spaces at the beginning or end - remove excess
+            _description = value?.Trim() ?? "Unknown";
+
+
+            // Must be at least 3 characters long (fill in the missing ones with #)
+            if (_description.Length < 3)
+                _description = _description.PadRight(3, '#');
+
+
+            // It can be a maximum of 15 characters (trim if too long, remove any spaces at the end)
+            if (_description.Length > 15)
+            {
+                _description = _description.Substring(0, 15).TrimEnd();
+
+
+                // Check if it has at least 3 characters after that and add # signs if necessary
+                if (_description.Length < 3)
+                    _description = _description.PadRight(3, '#');
+            }
+
+            // If the first character is a lowercase letter then convert it to uppercase
+            if (char.IsLower(_description[0]))
+                _description = char.ToUpper(_description[0]) + _description.Substring(1);
+        }
+    }
     public uint Size { get; set; } = 3;
 
 
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,11 +1,55 @@
 ﻿namespace Simulator;
  public class Creature
  {
+    // Default value "Unknown"
+    private string _name = "Unknown";
+    
+    private int _level = 1;
 
-    // Automatic properties
-    public string Name { get; set; }
-    public int Level { get; set; }
+    public string Name 
+    {
+        get => _name;
+        set
+        {   // Can only be assigned once upon initiation
+            if (_name != "Unknown") return;
 
+            // Cannot have spaces at the beginning or end - remove excess
+            _name = value?.Trim() ?? "Unknown";
+
+            // Must be at least 3 characters long (fill in the missing ones with #)
+            if (_name.Length < 3)
+                _name = _name.PadRight(3, '#');
+
+            // It can be a maximum of 25 characters (trim if too long, remove any spaces at the end)
+            if (_name.Length > 25)
+            {
+                _name = _name.Substring(0,25).TrimEnd();
+
+                // Check if it has at least 3 characters after that and add # signs if necessary
+                if (_name.Length < 3)
+                    _name = _name.PadRight(3, '#');
+            }
+
+            // If the first character is a lowercase letter then convert it to uppercase
+            if (char.IsLower(_name[0]))
+                _name = char.ToUpper(_name[0]) + _name.Substring(1);
+        }
+    }
+    public int Level
+    {
+        get => _level;
+        set
+        {
+            if (_level == 1)
+            {
+                if (value < 1)
+                    _level = 1;
+                else if (value > 10)
+                    _level = 10;
+                else _level = value;
+            }
+        }
+    }
 
     // Constructor that accepts initial values ​​for Name and Level
     public Creature(string name, int level = 1)
@@ -29,4 +73,12 @@
 
     // Readonly property Info
     public string Info => $"{Name} [{Level}]";
+
+
+    // Upgrade() method
+    public void Upgrade() 
+    {
+        if (Level < 10)
+            Level++;
+    }
 }
